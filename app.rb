78a22e70250d264e505238b9b91afc02c62d0aa5
weed <weed_7777@yahoo.co.jp>
2012-09-02 15:05:05 +0900

    @urlが空だったから？

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require 'haml'
 
 get '/' do
   haml :index
+  @url = ''
 end
 
 get '/output' do
+  haml :index
   @url = "https://www.google.co.jp/search?hl=ja&as_q=%s&as_sitesearch=" + params['url']
 end
