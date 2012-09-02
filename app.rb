87fe46d89e14ca9631b5575901f257f7c58c7c8f
weed <weed_7777@yahoo.co.jp>
2012-09-02 14:38:20 +0900

    URLエスケープした

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
+require 'cgi'
 
 get '/:url' do
-  "https://www.google.co.jp/search?hl=ja&as_q=%s&as_sitesearch=" + params['url']
+  "https://www.google.co.jp/search?hl=ja&as_q=%s&as_sitesearch=" + CGI.escape(params['url'])
 end
